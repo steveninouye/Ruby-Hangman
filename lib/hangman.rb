aac32c29e1fbf015ac1f60786ac343dab218be6f
Steven Inouye <steveninouye@msn.com>
2018-09-17 22:35:21 -1000

    started assignment

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,5 +1,11 @@
 class Hangman
   attr_reader :guesser, :referee, :board
+
+  def initialize (guesser, referee, board)
+    @guesser = guesser
+    @referee = referee
+    @board = board
+  end
 end
 
 class HumanPlayer
