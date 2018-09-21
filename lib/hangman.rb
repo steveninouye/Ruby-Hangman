0223db9a79431e88e80fabb47545b9d5e030dd9f
Steven Inouye <steveninouye@msn.com>
2018-09-20 22:39:08 -0400

    started play method on game

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,7 +4,13 @@ class Hangman
   def initialize (players)
     @guesser = players[:guesser]
     @referee = players[:referee]
-    @board = 0
+    @guesses_available = 6
+  end
+
+  def play
+    while @guesses_available > 0
+      
+    end
   end
 
   def setup
@@ -34,7 +40,14 @@ class HumanPlayer
     # diplays the board and returns the user's guess
   end
 
-  def handle_response
+  def handle_response(guess, idx)
+    str = idx.reduce("") {|a,c| a + "," + (c+1).to_s}
+    p "The letter #{guess} was found at #{str}"
+  end
+
+  def pick_secret_word
+    # displays directions to user to pick lenght of word
+    # gets user input and verifies it is an valid input
   end
 end
 
@@ -51,9 +64,8 @@ class ComputerPlayer
   end
 
   def check_guess (ltr)
-    word = @dictionary[0]
     result = []
-    word.chars.each_with_index do |el,idx|
+    @secret_word.each_with_index do |el,idx|
       result.push(idx) if ltr == el
     end
     result
@@ -68,7 +80,7 @@ class ComputerPlayer
   end
 
   def handle_response (ltr, arr)
-
+    arr
   end
 
   def candidate_words
